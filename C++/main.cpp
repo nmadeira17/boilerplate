db4b7dcd70467b54a99e06494a2405aaadad55aa
Nick Madeira <nmadeira17.aux@gmail.com>
2022-09-03 13:35:04 -0700

    Adding C++ boilerplate

---
+++ C++/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{
+   std::vector<std::string> vec { "Hello", " world!"};
+   for (const std::string & str : vec)
+   {
+      std::cout << str;
+   }
+   std::cout << std::endl;
+   
+   return 0;
+}
